b3bf5d6ee82df531fdbf7e0b7df5f0bdd0701242
Ethan Coyle <ethan.coyle@unb.ca>
2026-08-22 16:42:04 -0300

    good?

--- tb/axis_mac_xmii_tx/axis_mac_xmii_tx_tb.py
+++ tb/axis_mac_xmii_tx/axis_mac_xmii_tx_tb.py
@@ -18,22 +18,86 @@ sys.path.append(helper_path)
 
 from helper import axis_source
 
+from cocotb.handle import LogicObject
+from cocotb.types import LogicArray
+from cocotb.simtime import get_sim_time
+
+# class mii_source:
+#     def __init__(self, mii_tx_clk:LogicObject, mii_txd:LogicArray, mii_tx_en:LogicArray):
+#         self.mii_tx_clk = mii_tx_clk
+#         self.mii_txd = mii_txd
+#         self.mii_tx_en = mii_tx_en
+#         cocotb.start_soon(self.__axis_source__())
+
+#     def send_nowait(self, data, dest=[]):
+#         """Drives tdata and tdest with the values passed by the lists 'data' and 'dest'
+#         """
+#         for i in range(len(data)):
+#             self.tdata_queue.put_nowait(data[i])
+#             if (i == len(data) - 1):
+#                 self.tlast_queue.put_nowait(1)
+#             else:
+#                 self.tlast_queue.put_nowait(0)
+#         if (self.s_axis_tdest != None):
+#             for i in range(len(dest)):
+#                 self.tdest_queue.put_nowait(dest[i])
+#         self.tdata_present.set()
+
+#     async def __mii_source__(self):
+
+class mii_sink:
+    def __init__(self, mii_tx_clk:LogicObject, mii_txd:LogicArray, mii_tx_en:LogicArray):
+        self.mii_tx_clk = mii_tx_clk
+        self.mii_txd = mii_txd
+        self.mii_tx_en = mii_tx_en
+        self.packets_received = []
+        self.bytes_received = []
+        cocotb.start_soon(Clock(self.mii_tx_clk, 20, unit="ns").start())
+        cocotb.start_soon(self.__mii_sink__())
+
+    async def __mii_sink__(self):
+        count = 0
+        receiving = False
+        while (True):
+            await RisingEdge(self.mii_tx_clk)
+            if (self.mii_tx_en.value):
+                receiving = True
+                count = count + 1
+                if (count == 1):
+                    byte = int(self.mii_txd.value)
+                if (count == 2):
+                    count = 0
+                    byte = byte | (int(self.mii_txd.value) << 4)
+                    self.bytes_received.append(byte)
+            else:
+                count = 0
+                if (receiving):
+                    receiving = False
+                    # for 
+                    self.packets_received.append(self.bytes_received)
+
 @cocotb.test()
 async def test(dut):
-
-    cocotb.start_soon(Clock(dut.xmii_tx_clk, 20, unit="ns").start())
-
-    src = axis_source(dut.xmii_tx_clk, dut.s_axis_tdata, dut.s_axis_tvalid, dut.s_axis_tready, dut.s_axis_tlast)
+    axis_src = axis_source(dut.xmii_tx_clk, dut.s_axis_tdata, dut.s_axis_tvalid, dut.s_axis_tready, dut.s_axis_tlast)
+    mii_snk = mii_sink(dut.xmii_tx_clk, dut.xmii_txd, dut.xmii_tx_en)
 
     await RisingEdge(dut.xmii_tx_clk)
 
     data = []
-    for i in range(60):
+    for i in range(20):
+        print(hex(i))
         data += [i]
 
-    src.send_nowait(data)
+    axis_src.send_nowait(data)
 
-    await Timer(10000, unit='ns')
+    # for i in range(200):
+    #     await RisingEdge(dut.xmii_tx_clk)
+        # if (dut.xmii_tx_en.value):
+        #     print(hex(int(dut.xmii_txd.value)))
+
+    await Timer(3000, unit='ns')
+
+    print([hex(x) for x in mii_snk.packets_received[0]])
 
     # Be sure to place meaningful assertions in your tests. This is just here as an example of a test that will pass.
     assert 1 == 1
