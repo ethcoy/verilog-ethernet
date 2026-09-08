8450923d59e4abf4b7acc75b04b1f97f17ae7a6c
Ethan Coyle <ethan.coyle@unb.ca>
2026-09-08 17:03:13 -0300

    vivado copied into project fixing code

--- examples/counter/boards/nexys_a7_100t/fpga.sv
+++ examples/counter/boards/nexys_a7_100t/fpga.sv
@@ -5,22 +5,43 @@ module fpga #(
     input wire logic i_rst,
 
     // xMII Tx interface
-    input wire logic xmii_tx_clk,
+    output wire logic xmii_tx_clk_45,
     output wire logic [XMII_WIDTH - 1:0] xmii_txd,
     output wire logic xmii_tx_en,
-    output wire logic xmii_tx_er
+    
+    output wire logic [1:0] xmii_rxd,
+    // Need to actually add resets to each module...
+    // When reset is asserted, do not look at xmii_crsdv
+    output wire logic xmii_crsdv,
+    output wire logic xmii_rst
 );
+
+assign xmii_rxd[0] = (xmii_rst == 0) ? 1'b1 : 1'bz;
+assign xmii_rxd[1] = (xmii_rst == 0) ? 1'b1 : 1'bz;
+assign xmii_crsdv = (xmii_rst == 0) ? 1'b1 : 1'bz;
+
+assign xmii_rst = i_rst;
+
+wire xmii_tx_clk;
+wire clk_out3;
+
+clk_wiz_0 
+clk_wiz_0_inst (
+    .mmcm_xmii_tx_clk_0(xmii_tx_clk),
+    .mmcm_xmii_tx_clk_45(xmii_tx_clk_45),
+    .clk_out3(clk_out3),
+    .clk_100mhz_in(i_clk)
+ );
 
 axis_tcp_ip_stack #(
     .XMII_WIDTH(XMII_WIDTH)
 ) 
 axis_tcp_ip_stack_inst (
-    .i_clk(i_clk),
+    .i_clk(clk_out3),
     .i_rst(i_rst),
     .xmii_tx_clk(xmii_tx_clk),
     .xmii_txd(xmii_txd),
-    .xmii_tx_en(xmii_tx_en),
-    .xmii_tx_er(xmii_tx_er)
+    .xmii_tx_en(xmii_tx_en)
 );
 
 
